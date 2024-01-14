b0571bb15e492ad24d1414c6f0f5780d85714769
QF_tx4 <qf0129@qq.com>
2024-01-14 15:54:20 +0800

    init

---
+++ logx/log.go
@@ -0,0 +1,48 @@
+package logx
+
+import (
+	"context"
+	"log/slog"
+)
+
+var logger = slog.Default()
+
+func Debug(msg string, args ...any) {
+	logger.Debug(msg, args...)
+}
+
+func DebugContext(ctx context.Context, msg string, args ...any) {
+	logger.DebugContext(ctx, msg, args...)
+}
+
+func Info(msg string, args ...any) {
+	logger.Info(msg, args...)
+}
+
+func InfoContext(ctx context.Context, msg string, args ...any) {
+	logger.InfoContext(ctx, msg, args...)
+}
+
+func Warn(msg string, args ...any) {
+	logger.Warn(msg, args...)
+}
+
+func WarnContext(ctx context.Context, msg string, args ...any) {
+	logger.WarnContext(ctx, msg, args...)
+}
+
+func Error(msg string, args ...any) {
+	logger.Error(msg, args...)
+}
+
+func ErrorContext(ctx context.Context, msg string, args ...any) {
+	logger.ErrorContext(ctx, msg, args...)
+}
+
+func Log(ctx context.Context, level slog.Level, msg string, args ...any) {
+	logger.Log(ctx, level, msg, args...)
+}
+
+func LogAttrs(ctx context.Context, level slog.Level, msg string, attrs ...slog.Attr) {
+	logger.LogAttrs(ctx, level, msg, attrs...)
+}
