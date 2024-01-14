93d5f1f8d9125ac50eacc4db0ff4d9de4b4a3d1d
QF_tx4 <qf0129@qq.com>
2024-01-14 15:18:16 +0800

    init

--- modelx/base.go
+++ modelx/base.go
@@ -1,16 +1,14 @@
 package modelx
 
 import (
-	"time"
-
 	"github.com/rs/xid"
 	"gorm.io/gorm"
 )
 
 type BaseModel struct {
 	Id    string          `gorm:"primaryKey;type:varchar(50);" json:"id"`
-	Ctime *time.Time      `gorm:"autoCreateTime;comment:'CreatedTime'" json:"ctime"`
-	Utime *time.Time      `gorm:"autoUpdateTime;comment:'UpdatedTime'" json:"utime"`
+	Ctime *Time           `gorm:"autoCreateTime;comment:'CreatedTime'" json:"ctime"`
+	Utime *Time           `gorm:"autoUpdateTime;comment:'UpdatedTime'" json:"utime"`
 	Dtime *gorm.DeletedAt `gorm:"index;comment:'DeletedTime'" json:"-" `
 }
 
